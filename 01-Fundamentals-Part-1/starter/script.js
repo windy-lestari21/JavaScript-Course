7a026530fea319fb0d0802b02f18aa3fd3c1d0ee
windy-lestari <windyy.lestari11@gmail.com>
2025-03-05 14:13:19 +0800

    Wednesday, 5 Mar 2025

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -247,4 +247,133 @@ GOOD LUCK 😀
 
 // if (favourite !== 23) console.log("Why not 23?");
 
-// BOOLEAN
+// BOOLEAN-OPERATOR
+
+// const hasDriverLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriverLicense && hasGoodVision);
+// console.log(hasDriverLicense || hasGoodVision);
+// console.log(!hasDriverLicense);
+
+// if (hasDriverLicense && hasGoodVision) {
+//   console.log("You can drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+// const isTired = false;
+// console.log(hasDriverLicense && hasGoodVision && isTired);
+
+// if (hasDriverLicense && hasGoodVision && !isTired) {
+//   console.log("You can drive, but be careful!");
+// } else {
+//   console.log("You should stay home and relax!");
+// }
+
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win!");
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log("Koalas win!");
+// } else if (scoreKoalas === scoreDolphins) {
+//   console.log("It's a draw!");
+// }
+
+// BONUS 1
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log("Dolphins win!");
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log("Koalas win!");
+// } else if (
+//   scoreKoalas === scoreDolphins &&
+//   scoreKoalas >= 100 &&
+//   scoreDolphins >= 100
+// ) {
+//   console.log("It's a draw!");
+// } else {
+//   console.log("No one wins the trophy!:money_mouth_face:");
+// }
+
+// const day = "monday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("Monday is a cool day!");
+//     console.log("Go to school.");
+//     break;
+
+//   case "tuesday":
+//     console.log("prepare theory");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Go to work!");
+//   case "friday":
+//     console.log("Have fun!");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Sleep tight!");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// if (day === "monday") {
+//   console.log("Monday is a cool day!");
+//   console.log("Go to school.");
+// } else if (day === "tuesday") {
+//   console.log("prepare theory");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Go to work!");
+// } else if (day === "friday") {
+//   console.log("Have fun!");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Sleep tight!");
+// } else console.log("Invalid day");
+
+// statement and ekspression
+
+const age = 15;
+// age >= 18
+//   ? console.log("I like to drink wine ")
+//   : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log("I like to drink {age >= 18 ? 'wine : 'water'}");
