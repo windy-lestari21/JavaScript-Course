dd33bbb34bcf4554e5be04113cfbf3f1ed14a370
windy-lestari <windyy.lestari11@gmail.com>
2025-03-18 18:04:05 +0800

    Tuesday, 18 March 2025

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
